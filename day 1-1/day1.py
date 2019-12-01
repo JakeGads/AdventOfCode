e73c5ad175504801e721335e8215c7d8237c646e
Jake Gadaleta <jake.gads@gmail.com>
2019-12-01 09:30:42 -0500

    added comments

--- day 1-1/day1.py
+++ day 1-1/day1.py
@@ -4,19 +4,19 @@ Specifically, to find the fuel required for a module, take its mass, divide by t
 
 def calc_difference(mod_weight):
     current_weight = int(float(mod_weight) / 3) - 2 # weight of mass
-    weight = current_weight # weight of the 
-    while(True):
-        if int(float(current_weight) / 3) - 2 > 0:
-            current_weight = int(float(current_weight) / 3) - 2
-            weight += current_weight
-        else:
-            return weight
+    weight = current_weight # weight of current mod
+    while(True): # runs until return
+        if int(float(current_weight) / 3) - 2 > 0: # checks to see if it can move down another rung
+            current_weight = int(float(current_weight) / 3) - 2 # moves down that rung
+            weight += current_weight # adds that back to weight
+        else: 
+            return weight # if there is no more calc to do return the value
 
 
 if __name__ == "__main__":
-    summation = 0
-    with open('day1.txt', 'r') as file:
-        for line in file:
-            summation += calc_difference(line)
+    summation = 0 # the sum 
+    with open('day1.txt', 'r') as file: # opens the file
+        for line in file: # reads all lines
+            summation += calc_difference(line) # calcs 
     
-    print(summation)
+    print(summation) # outputs
