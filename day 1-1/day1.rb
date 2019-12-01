85e39ea11acc698fbb661baa0c3fbc356b9ef665
Jake Gadaleta <jake.gads@gmail.com>
2019-12-01 09:31:07 -0500

    added comments, set up main function

--- day 1-1/day1.rb
+++ day 1-1/day1.rb
@@ -1,34 +1,36 @@
 def hand_off(num)
-    number = num.to_f / 3
-    number = number.floor
-    number -= 2
+    # does the claculation step by step for debugging pruposes
+    number = num.to_f / 3 # forces float (regardless of current type)
+    number = number.floor # floors the number
+    number -= 2 # subtracts 2
 end
 
 def calc_difference(weight)
-    current_weight = hand_off(weight)
-    weight = current_weight
+    current_weight = hand_off(weight) # get the first weight
+    weight = current_weight # set the max weight
 
-    is_not_zero = true
-
-    while is_not_zero
-        if hand_off(current_weight) > 0
-            current_weight = hand_off(current_weight)
-            weight += current_weight       
+    while true # loop runs until returns
+        if hand_off(current_weight) > 0 # check if it is viable
+            current_weight = hand_off(current_weight) # recalculte current weight
+            weight += current_weight # recalculate weight
         else
-            return weight
+            return weight # if there is no more calcualtions return weight
         end
         
     end
 end
 
-sum = 0
+if __FILE__ == $0 # checks if this is the executing file
+    sum = 0
 
-file_data = File.open("day1.txt").readlines.map(&:chomp)
+    # opens file and splices it into an array
+    file_data = File.open("day1.txt").readlines.map(&:chomp)
 
-file_data = file_data.map {|val| val.to_f}
 
-for i in file_data do
-    sum += calc_difference(i)
-end
+    file_data = file_data.map {|val| val.to_f} # forces values to be floats
 
-puts sum
+    for i in file_data do # loops through all values calcuating difference
+        sum += calc_difference(i)
+    end
+
+    puts sum
